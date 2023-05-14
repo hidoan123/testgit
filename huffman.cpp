f27263c431efd48705cedd5ebc133871e13c4496
hidoan123 <nguyenvandoan30122001@gmail.com>
2023-05-14 15:05:17 +0700

    init project

---
+++ huffman.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct MinHeapNode
+{
+    char data;
+    unsigned freq;
+    MinHeapNode *left, *right;
+    MinHeapNode(char data, unsigned freq)
+    {
+        left = right = NULL;
+        this->data = data;
+        this->freq = freq;
+    }
+};
+
+struct compare
+{
+    bool operator()(MinHeapNode *l, MinHeapNode *r)
+    {
+        return (l->freq > r->freq);
+    }
+};
+
+void printCodes(struct MinHeapNode *root, string str)
+{
+    if (!root)
+        return;
+    if (root->data != '$')
+        cout << root->data << ": " << str << "\n";
+    printCodes(root->left, str + "0");
+    printCodes(root->right, str + "1");
+}
+
+void HuffmanCodes(char data[], int freq[], int size)
+{
+    struct MinHeapNode *left, *right, *top;
+    priority_queue<MinHeapNode *, vector<MinHeapNode *>, compare> minHeap;
+    for (int i = 0; i < size; ++i)
+        minHeap.push(new MinHeapNode(data[i], freq[i]));
+    while (minHeap.size() != 1)
+    {
+        left = minHeap.top();
+        minHeap.pop();
+        right = minHeap.top();
+        minHeap.pop();
+        top = new MinHeapNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        minHeap.push(top);
+    }
+    printCodes(minHeap.top(), "");
+}
+
+int main()
+{
+    char ch[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    int fr[] = {5, 9, 12, 13, 16, 45};
+    int size = sizeof(ch) / sizeof(ch[0]);
+    HuffmanCodes(ch, fr, size);
+    return 0;
+}
